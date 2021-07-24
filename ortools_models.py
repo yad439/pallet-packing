635633ff4ee3cdc47f74498a30d9b1ebbc077ff4
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-07-24 17:16:41 +0700

    Extract common model creation

--- ortools_models.py
+++ ortools_models.py
@@ -1,91 +1,55 @@
 from ortools.linear_solver import pywraplp
 
 
-def task_one_model(pallet_width, pallet_height, items, *, limit=0):
-    solver = pywraplp.Solver.CreateSolver('SCIP')
-    B = sum(i[0] + i[1] for i in items)
-    n = len(items)
-    h = list(map(lambda i: i[1], items))
-    w = list(map(lambda i: i[0], items))
-
-    x = [solver.NumVar(0, solver.infinity(), f'x{i}') for i in range(n)]
-    y = [solver.NumVar(0, solver.infinity(), f'y{i}') for i in range(n)]
-    l = [[solver.BoolVar(f'l{i},{j}') for j in range(n)] for i in range(n)]
-    b = [[solver.BoolVar(f'b{i},{j}') for j in range(n)] for i in range(n)]
-    r = [solver.BoolVar(f'r{i}') for i in range(n)]
-    z = [solver.BoolVar(f'z{i}') for i in range(n)]
-
-    for i in range(n):
-        solver.Add(x[i] + w[i] * (1 - r[i]) + h[i] * r[i] <= pallet_width + (1 - z[i]) * B)
-        solver.Add(y[i] + h[i] * (1 - r[i]) + w[i] * r[i] <= pallet_height + (1 - z[i]) * B)
-        for j in range(n):
-            if i != j:
-                solver.Add(x[i] + w[i] * (1 - r[i]) + h[i] * r[i] <= x[j] + (1 - l[i][j]) * B)
-                solver.Add(y[i] + h[i] * (1 - r[i]) + w[i] * r[i] <= y[j] + (1 - b[i][j]) * B)
-                solver.Add(l[i][j] + l[j][i] + b[i][j] + b[j][i] >= 1)
-
-    solver.Minimize(pallet_height * pallet_width - sum(h[i] * w[i] * z[i] for i in range(n)))
+def task_one_model(pallet_width, pallet_height, items, *, limit=0, backend='SCIP'):
+    solver = _build_simple_model(pallet_width, pallet_height, items, backend)
 
     if limit != 0:
         solver.SetTimeLimit(limit)
 
-    solver.EnableOutput()
-
     status = solver.Solve()
 
-    zv = [bool(t.solution_value()) for t in z]
-    xv = [t.solution_value() for t in x]
-    yv = [t.solution_value() for t in y]
-    rv = [bool(t.solution_value()) for t in r]
+    n = len(items)
+    zv = [bool(solver.LookupVariable(f'z{i}').solution_value()) for i in range(n)]
+    xv = [solver.LookupVariable(f'x{i}').solution_value() for i in range(n)]
+    yv = [solver.LookupVariable(f'y{i}').solution_value() for i in range(n)]
+    rv = [bool(solver.LookupVariable(f'r{i}').solution_value()) for i in range(n)]
 
     return status, solver.Objective().Value(), solver.Objective().BestBound(), list(zip(zv, xv, yv, rv))
 
 
-def task_two_model(pallet_width, pallet_height, items, x_tolerance, y_tolerance, *, limit=0):
-    solver = pywraplp.Solver.CreateSolver('SCIP')
-    B = sum(i[0] + i[1] for i in items)
+# wrong model
+def task_two_model(pallet_width, pallet_height, items, x_tolerance, y_tolerance, *, limit=0, backend='SCIP'):
+    solver = _build_simple_model(pallet_width, pallet_height, items, backend)
     n = len(items)
-    h = list(map(lambda i: i[1], items))
-    w = list(map(lambda i: i[0], items))
-
-    x = [solver.NumVar(0, solver.infinity(), f'x{i}') for i in range(n)]
-    y = [solver.NumVar(0, solver.infinity(), f'y{i}') for i in range(n)]
-    l = [[solver.BoolVar(f'l{i},{j}') for j in range(n)] for i in range(n)]
-    b = [[solver.BoolVar(f'b{i},{j}') for j in range(n)] for i in range(n)]
-    r = [solver.BoolVar(f'r{i}') for i in range(n)]
-    z = [solver.BoolVar(f'z{i}') for i in range(n)]
+    big_num = sum(i[0] + i[1] for i in items)
+    x = [solver.LookupVariable(f'x{i}') for i in range(n)]
+    y = [solver.LookupVariable(f'y{i}') for i in range(n)]
+    z = [solver.LookupVariable(f'z{i}') for i in range(n)]
+    r = [solver.LookupVariable(f'r{i}') for i in range(n)]
     cx = [solver.NumVar(0, solver.infinity(), f'x{i}') for i in range(n)]
     cy = [solver.NumVar(0, solver.infinity(), f'y{i}') for i in range(n)]
 
     for i in range(n):
-        solver.Add(x[i] + w[i] * (1 - r[i]) + h[i] * r[i] <= pallet_width + (1 - z[i]) * B)
-        solver.Add(y[i] + h[i] * (1 - r[i]) + w[i] * r[i] <= pallet_height + (1 - z[i]) * B)
-        solver.Add(cx[i] <= B * z[i])
-        solver.Add(cy[i] <= B * z[i])
+        solver.Add(cx[i] <= big_num * z[i])
+        solver.Add(cy[i] <= big_num * z[i])
         solver.Add(cx[i] <= x[i])
         solver.Add(cy[i] <= y[i])
-        solver.Add(cx[i] >= x[i] - B * (1 - z[i]))
-        solver.Add(cy[i] >= y[i] - B * (1 - z[i]))
+        solver.Add(cx[i] >= x[i] - big_num * (1 - z[i]))
+        solver.Add(cy[i] >= y[i] - big_num * (1 - z[i]))
         for j in range(n):
             if i != j:
-                solver.Add(x[i] + w[i] * (1 - r[i]) + h[i] * r[i] <= x[j] + (1 - l[i][j]) * B)
-                solver.Add(y[i] + h[i] * (1 - r[i]) + w[i] * r[i] <= y[j] + (1 - b[i][j]) * B)
-                solver.Add(l[i][j] + l[j][i] + b[i][j] + b[j][i] >= 1)
-    solver.Add(sum(items[i][2] * cx[i] for i in range(n)) <= (pallet_width / 2 + x_tolerance) * sum(
-            items[i][2] * z[i] for i in range(n)))
-    solver.Add(sum(items[i][2] * cx[i] for i in range(n)) >= (pallet_width / 2 - x_tolerance) * sum(
-            items[i][2] * z[i] for i in range(n)))
-    solver.Add(sum(items[i][2] * cy[i] for i in range(n)) <= (pallet_width / 2 + y_tolerance) * sum(
-            items[i][2] * z[i] for i in range(n)))
-    solver.Add(sum(items[i][2] * cy[i] for i in range(n)) >= (pallet_width / 2 - y_tolerance) * sum(
-            items[i][2] * z[i] for i in range(n)))
-
-    solver.Minimize(pallet_height * pallet_width - sum(h[i] * w[i] * z[i] for i in range(n)))
+                solver.Add(sum(items[i][2] * cx[i] for i in range(n)) <= (pallet_width / 2 + x_tolerance) * sum(
+                        items[i][2] * z[i] for i in range(n)))
+                solver.Add(sum(items[i][2] * cx[i] for i in range(n)) >= (pallet_width / 2 - x_tolerance) * sum(
+                        items[i][2] * z[i] for i in range(n)))
+                solver.Add(sum(items[i][2] * cy[i] for i in range(n)) <= (pallet_width / 2 + y_tolerance) * sum(
+                        items[i][2] * z[i] for i in range(n)))
+                solver.Add(sum(items[i][2] * cy[i] for i in range(n)) >= (pallet_width / 2 - y_tolerance) * sum(
+                        items[i][2] * z[i] for i in range(n)))
 
     if limit != 0:
         solver.SetTimeLimit(limit)
-
-    solver.EnableOutput()
 
     status = solver.Solve()
 
@@ -95,3 +59,27 @@ def task_two_model(pallet_width, pallet_height, items, x_tolerance, y_tolerance,
     rv = [bool(t.solution_value()) for t in r]
 
     return status, solver.Objective().Value(), solver.Objective().BestBound(), list(zip(zv, xv, yv, rv))
+
+
+def _build_simple_model(pallet_width, pallet_height, items, backend):
+    solver = pywraplp.Solver.CreateSolver(backend)
+    big_num = sum(i[0] + i[1] for i in items)
+    n = len(items)
+    h = list(map(lambda i: i[1], items))
+    w = list(map(lambda i: i[0], items))
+    x = [solver.NumVar(0, solver.infinity(), f'x{i}') for i in range(n)]
+    y = [solver.NumVar(0, solver.infinity(), f'y{i}') for i in range(n)]
+    l = [[solver.BoolVar(f'l{i},{j}') for j in range(n)] for i in range(n)]
+    b = [[solver.BoolVar(f'b{i},{j}') for j in range(n)] for i in range(n)]
+    r = [solver.BoolVar(f'r{i}') for i in range(n)]
+    z = [solver.BoolVar(f'z{i}') for i in range(n)]
+    for i in range(n):
+        solver.Add(x[i] + w[i] * (1 - r[i]) + h[i] * r[i] <= pallet_width + (1 - z[i]) * big_num)
+        solver.Add(y[i] + h[i] * (1 - r[i]) + w[i] * r[i] <= pallet_height + (1 - z[i]) * big_num)
+        for j in range(n):
+            if i != j:
+                solver.Add(x[i] + w[i] * (1 - r[i]) + h[i] * r[i] <= x[j] + (1 - l[i][j]) * big_num)
+                solver.Add(y[i] + h[i] * (1 - r[i]) + w[i] * r[i] <= y[j] + (1 - b[i][j]) * big_num)
+                solver.Add(l[i][j] + l[j][i] + b[i][j] + b[j][i] >= 1)
+    solver.Minimize(pallet_height * pallet_width - sum(h[i] * w[i] * z[i] for i in range(n)))
+    return solver
