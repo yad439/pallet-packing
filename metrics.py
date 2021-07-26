a441711cbbf64d6db033a7046f14336dbca03622
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-07-26 19:54:44 +0700

    Fix message in efficiency print

--- metrics.py
+++ metrics.py
@@ -10,4 +10,4 @@ def print_efficiency(pallet_width: int, pallet_height: int, items: List[Item],
     result = algorithm(pallet_width, pallet_height, items)
     end_time = time.time()
     print('Execution took:', end_time - start_time, 'seconds\nResult is:', result[0], '-',
-          '{:.2%}'.format(result[0] / (pallet_width * pallet_height)), 'of pallet used')
+          '{:.2%}'.format(result[0] / (pallet_width * pallet_height)), 'of pallet is free')
