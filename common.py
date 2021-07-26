3c95b67db8d82c78fff0eb6d66b13daedad25156
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-07-26 14:15:23 +0700

    Add string representation methods to item and position

--- common.py
+++ common.py
@@ -7,9 +7,21 @@ class Item:
         self.height = height
         self.mass = mass
 
+    def __repr__(self) -> str:
+        return f'Item({repr(self.width)}, {repr(self.height)}, {repr(self.mass)})'
+
+    def __str__(self) -> str:
+        return f'Item({str(self.width)}, {str(self.height)}, {str(self.mass)})'
+
 
 class Position:
     def __init__(self, x: Union[int, float], y: Union[int, float], rotated: bool):
         self.x = x
         self.y = y
         self.rotated = rotated
+
+    def __repr__(self) -> str:
+        return f'Position({repr(self.x)}, {repr(self.y)}, {repr(self.rotated)})'
+
+    def __str__(self) -> str:
+        return f'Position({str(self.x)}, {str(self.y)}, {str(self.rotated)})'
