d3eaf7213973526467665079548164837ce10adb
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-07-26 15:13:09 +0700

    Implement item rotation in skyline

--- skyline.py
+++ skyline.py
@@ -32,6 +32,7 @@ def skyline_decode(pallet_width: int, pallet_height: int, items: List[Item], per
 
         best_item = None
         score = -1
+        rotated = False
         for current_item in items_list:
             if current_item.data[1] <= pallet_height - gap.data[1] and current_item.data[0] <= width:
                 cur_score = int(current_item.data[1] == height1) + int(current_item.data[1] == height2) + int(
@@ -39,7 +40,17 @@ def skyline_decode(pallet_width: int, pallet_height: int, items: List[Item], per
                 if cur_score > score:
                     score = cur_score
                     best_item = current_item
+                    rotated = False
+            if current_item.data[0] <= pallet_height - gap.data[1] and current_item.data[1] <= width:
+                cur_score = int(current_item.data[0] == height1) + int(current_item.data[0] == height2) + int(
+                        current_item.data[1] == width)
+                if cur_score > score:
+                    score = cur_score
+                    best_item = current_item
+                    rotated = True
 
+        best_item_width = (best_item.data[0] if not rotated else best_item.data[1]) if best_item is not None else None
+        best_item_height = (best_item.data[1] if not rotated else best_item.data[0]) if best_item is not None else None
         if score == -1:
             if height1 <= height2:
                 if gap is not skyline.first:
@@ -59,38 +70,38 @@ def skyline_decode(pallet_width: int, pallet_height: int, items: List[Item], per
                     gap.data = 0, gap.data[1] + height2
                     skyline.remove(gap.next)
         elif score == 0:
-            results[best_item.data[2]] = Position(gap.data[0], gap.data[1], False)
+            results[best_item.data[2]] = Position(gap.data[0], gap.data[1], rotated)
 
-            skyline.insert(gap, (gap.data[0] + best_item.data[0], gap.data[1]))
-            gap.data = gap.data[0], gap.data[1] + best_item.data[1]
+            skyline.insert(gap, (gap.data[0] + best_item_width, gap.data[1]))
+            gap.data = gap.data[0], gap.data[1] + best_item_height
 
             items_list.remove(best_item)
         elif score == 1:
-            if best_item.data[1] == height1:
-                results[best_item.data[2]] = Position(gap.data[0], gap.data[1], False)
-                gap.data = gap.data[0] + best_item.data[0], gap.data[1]
-            elif best_item.data[1] == height2:
+            if best_item_height == height1:
+                results[best_item.data[2]] = Position(gap.data[0], gap.data[1], rotated)
+                gap.data = gap.data[0] + best_item_width, gap.data[1]
+            elif best_item_height == height2:
                 next_point = gap.next
                 if next_point is not None:
-                    results[best_item.data[2]] = Position(next_point.data[0] - best_item.data[0], gap.data[1], False)
-                    next_point.data = next_point.data[0] - best_item.data[0], next_point.data[1]
+                    results[best_item.data[2]] = Position(next_point.data[0] - best_item_width, gap.data[1], rotated)
+                    next_point.data = next_point.data[0] - best_item_width, next_point.data[1]
                 else:
-                    results[best_item.data[2]] = Position(pallet_width - best_item.data[0], gap.data[1], False)
-                    skyline.add((pallet_width - best_item.data[0], gap.data[1] + best_item.data[1]))
+                    results[best_item.data[2]] = Position(pallet_width - best_item_width, gap.data[1], rotated)
+                    skyline.add((pallet_width - best_item_width, gap.data[1] + best_item_height))
             else:
-                results[best_item.data[2]] = Position(gap.data[0], gap.data[1], False)
-                gap.data = gap.data[0], gap.data[1] + best_item.data[1]
+                results[best_item.data[2]] = Position(gap.data[0], gap.data[1], rotated)
+                gap.data = gap.data[0], gap.data[1] + best_item_height
 
             items_list.remove(best_item)
         elif score == 2:
-            results[best_item.data[2]] = Position(gap.data[0], gap.data[1], False)
-            if best_item.data[1] != height1:
+            results[best_item.data[2]] = Position(gap.data[0], gap.data[1], rotated)
+            if best_item_height != height1:
                 skyline.remove(gap.next)
-                gap.data = gap.data[0], gap.data[1] + best_item.data[1]
-            elif best_item.data[1] != height2:
+                gap.data = gap.data[0], gap.data[1] + best_item_height
+            elif best_item_height != height2:
                 skyline.remove(gap)
             else:
-                gap.data = gap.data[0] + best_item.data[0], gap.data[1]
+                gap.data = gap.data[0] + best_item_width, gap.data[1]
 
             items_list.remove(best_item)
         elif score == 3:
@@ -99,7 +110,7 @@ def skyline_decode(pallet_width: int, pallet_height: int, items: List[Item], per
             skyline.remove(next_point)
 
             items_list.remove(best_item)
-            results[best_item.data[2]] = Position(gap.data[0], gap.data[1], False)
+            results[best_item.data[2]] = Position(gap.data[0], gap.data[1], rotated)
 
     packed = 0
     for i in range(len(items)):
