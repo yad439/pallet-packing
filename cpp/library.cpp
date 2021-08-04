e2dd1506c0b74691689447554ac57704e5f955a4
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-08-04 15:54:36 +0700

    Add header for exported functions

--- cpp/library.cpp
+++ cpp/library.cpp
@@ -1,3 +1,5 @@
+#include "library.h"
+
 #include <ranges>
 #include <vector>
 #include <numeric>
@@ -9,7 +11,7 @@ using std::span;
 extern "C" {
 [[maybe_unused]] __declspec(dllexport) int __cdecl simple_skyline(
 		int pallet_width, int pallet_height, int n, Item *items, Position *positions
-                                                                 ) noexcept {
+                                                                 ) {
 	std::vector<size_t> permutation(static_cast<std::vector<size_t>::size_type>(n));
 	std::iota(permutation.begin(), permutation.end(), 0);
 	return skyline_decode(pallet_width, pallet_height, span(items, static_cast<std::span<Item>::size_type>(n)),
---
+++ cpp/library.h
@@ -0,0 +1,12 @@
+#ifndef PALLET_PACKING_CPP_LIBRARY_H
+#define PALLET_PACKING_CPP_LIBRARY_H
+
+#include "common.h"
+
+extern "C" {
+[[maybe_unused]] __declspec(dllexport) int __cdecl simple_skyline(
+		int pallet_width, int pallet_height, int n, Item *items, Position *positions
+                                                                 );
+}
+
+#endif //PALLET_PACKING_CPP_LIBRARY_H
