e80c088c5f124dd92298147a2b8fcba96b798bb3
AkimovAl <87817886+AkimovAl@users.noreply.github.com>
2021-08-13 19:53:53 +0700

    Update simulated_annealing.h

--- cpp/simulated_annealing.h
+++ cpp/simulated_annealing.h
@@ -3,4 +3,4 @@
 #include "side_fucnctions.h"
 #include<math.h>
 using namespace std;
-void simulated_annealing(int H, int W, double r);
+void simulated_annealing(int H, int W, double r, int num_of_iters);
