0e1642341a3d73701735c99dbd174a2372aa6ae9
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-08-13 11:33:49 +0700

    Refactor concat wrapper: remove redundant parameter

--- algorithm_wrappers.py
+++ algorithm_wrappers.py
@@ -1,27 +1,24 @@
-from typing import Iterable, List, Optional, Tuple
+from typing import List, Optional, Tuple
 
 from common import Item, Position
+from concat_baseline.concat_baseline import Rectangle, assign_coordinates, pallet_placement, rects_flatten
 
-from concat_baseline.concat_baseline import Rectangle, pallet_placement, assign_coordinates, rects_flatten
 
-
-def concat_placement(pallet_width: int, pallet_height: int, items: List[Item],
-                     permutation: Optional[Iterable[int]] = None) -> Tuple[int, List[Optional[Position]]]:
-    
+def concat_placement(pallet_width: int, pallet_height: int, items: List[Item]) -> Tuple[int, List[Optional[Position]]]:
     rects = [Rectangle(item.width, item.height)
              for item in items
-            ]
-    
-    pallet = Rectangle(pallet_width, pallet_height,[], (pallet_width, pallet_height))
-    
+             ]
+
+    pallet = Rectangle(pallet_width, pallet_height, [], (pallet_width, pallet_height))
+
     rest_rects = pallet_placement(pallet, rects)
     assert len(rest_rects) + len(rects_flatten(pallet.placed)) == len(rects)
 
     solve_xywh = assign_coordinates(0, 0, pallet.w, pallet.h, pallet.placed)
-    
+
     results = [None] * len(items)
-    for i,item in enumerate(items):
-        for j, (x,y,w,h) in enumerate(solve_xywh):
+    for i, item in enumerate(items):
+        for j, (x, y, w, h) in enumerate(solve_xywh):
             if item.width == w and item.height == h:
                 results[i] = Position(x, y, False)
                 del solve_xywh[j]
@@ -31,15 +28,5 @@ def concat_placement(pallet_width: int, pallet_height: int, items: List[Item],
                 del solve_xywh[j]
                 break
     assert not solve_xywh, f'solve_xywh={solve_xywh}'
-            
+
     return pallet.free_square, results
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
