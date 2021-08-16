fcf099d08ac5b8680de5882528c3538c973ec0e4
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-08-16 16:43:30 +0700

    Add python build script

---
+++ build.py
@@ -0,0 +1,25 @@
+import platform
+import shutil
+import subprocess
+import sys
+
+
+def build_with_cmake():
+    build_dir = 'cpp/build'
+    src_dir = 'cpp'
+    cmake = 'cmake'
+
+    cmake_generator = []
+    if len(sys.argv) == 2:
+        cmake_generator = ['-G', sys.argv[1]]
+    elif platform.system() == 'Windows':
+        if subprocess.run([cmake, src_dir, '-B', build_dir]).returncode != 0:
+            cmake_generator = ['-G', 'MinGW Makefiles']
+            shutil.rmtree(build_dir)
+
+    subprocess.run([cmake, src_dir, '-B', build_dir, '-DCMAKE_BUILD_TYPE=Release'] + cmake_generator)
+    subprocess.run([cmake, '--build', build_dir, '--config', 'Release', '--target', 'install'])
+
+
+if __name__ == '__main__':
+    build_with_cmake()
