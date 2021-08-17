74948e7c3e796badc57ec898a6e038754f1d64fb
AkimovAl <87817886+AkimovAl@users.noreply.github.com>
2021-08-17 14:24:43 +0700

    Update side_fucnctions.h

--- cpp/side_fucnctions.h
+++ cpp/side_fucnctions.h
@@ -5,6 +5,7 @@
 using namespace std;
 string create_simple_string(int n);
 string create_simple_string1(int n);
+string create_nfd_string(vector<Data*> &rectangles, int H, int W);
 void pushrect(vector<Data*>& rectangles);
 void display(const string& T, int S, const vector<Data*>& rectangles, const vector<point*>& positions, double sec);
 string neighborhood(string T, vector<Data*>& rectangles);
